6956796c4f62a4a1046625628b12e5bb41f5f2f7
Edward <wcamp@121g.io>
2023-01-24 21:31:38 -0500

    Initial Commit

---
+++ CityReports/CityReportsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CityReportsApp.swift
+//  CityReports
+//
+//  Created by Walker on 1/24/23.
+//
+
+import SwiftUI
+
+@main
+struct CityReportsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

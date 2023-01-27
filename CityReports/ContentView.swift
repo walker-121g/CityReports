fc98df29c33e7077244d589e9ea52e36a3367717
Edward <wcamp@121g.io>
2023-01-27 16:43:04 -0500

    initialization

--- CityReports/ContentView.swift
+++ CityReports/ContentView.swift
@@ -1,16 +1,9 @@
-//
-//  ContentView.swift
-//  CityReports
-//
-//  Created by Walker on 1/24/23.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
+        HStack {
+            Image(systemName: "person")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
